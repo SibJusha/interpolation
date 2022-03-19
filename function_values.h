65ed68be5376bb76801a597ac09c1af5c5f86885
Andrjusha <andreas.yushac@gmail.com>
2022-03-19 22:56:19 +0700

    all ok

--- function_values.h
+++ function_values.h
@@ -6,6 +6,7 @@
 double get_value(double x)
 {
     return sin(x);
+    // return 1/(1+25*x*x);
 }
 
 #endif //INTERPOL_FUNCTION_VALUES_H
--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 #include <vector>
 #include <fstream>
 #include <cmath>
+#include <algorithm>
 #include "function_values.h"
 
 void get_coefficients (std::vector<double>& coefficients, int n,
                        std::vector<double>& X)
 {
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i <= n; i++) {
         double coeff = 0;
         for (int j = 0; j <= i; j++) {
             double multi = 1;
@@ -23,14 +24,10 @@ void get_coefficients (std::vector<double>& coefficients, int n,
 }
 
 double get_value (std::vector<double>& coeffs, std::vector<double>& X,
-                  double x)
+                  double x, int n)
 {
-    int n = coeffs.size();
-    double result = 0;
-    if (n > 0) {
-        result += coeffs[0];
-    }
-    for (int i = 1; i < n; i++) {
+    double result = coeffs[0];
+    for (int i = 1; i <= n; i++) {
         double multi = 1;
         for (int j = 0; j < i; j++) {
             multi *= (x - X[j]);
@@ -45,34 +42,50 @@ int main() {
     std::ofstream out, debug;
     in.open("input.txt");
     debug.open("debug.txt");
-    out.open("output.txt", std::ios::ate);
+    out.open("output.txt");
 
     int n1, n2;
     double a, b, H, h;
-    bool cheby;
-    in >> cheby >> a >> b >> n1 >> n2;
+    in >> a >> b >> n1 >> n2;
     std::vector<double> variable;
-    if (cheby) {
-        for (int i = 0; i < n1; i++) {
-            double x = (a + b) / 2 + (b - a) / 2 * cos((2*i + 1)*M_PI/2/(n1+1));
-            debug << x << std::endl;
-            variable.insert(variable.end(), x);
-        }
-    } else {
-        H = (b - a) / n1;
-        for (int i = 0; i < n1; i++) {
-            variable.insert(variable.end(), a + i * H);
-        }
+    H = (b - a) / n1;
+    for (int i = 0; i <= n1; i++) {
+        debug << a + i*H << std::endl;
+        variable.insert(variable.end(), a + i * H);
     }
     h = (b - a) / n2;
     std::vector<double> coefficients;
     get_coefficients(coefficients, n1, variable);
     double max = -1;
     double max_x = -1;
-    for (int i = 0; i < n2; i++) {
-        double x = get_value(coefficients, variable, a + i * h);
+    for (int i = 0; i <= n2; i++) {
+        double x = get_value(coefficients, variable, a + i * h, n2);
         debug << a + i * h << "\t\t" << x << std::endl;
         double y = get_value(a + i * h);
+        if (std::abs(y - x) > max) {
+            max = std::abs(y - x);
+            max_x = a + i * h;
+        }
+    }
+
+    out << max << std::endl << "x = "<< max_x << std::endl;
+
+    variable.erase(variable.begin(), variable.end());
+    coefficients.erase(coefficients.cbegin(), coefficients.cend());
+    for (int i = 0; i <= n1; i++) {
+        double x = ((a + b) + (b - a) * cos((2*i + 1)*M_PI/(2*(n1+1))))/2;
+        debug << x << std::endl;
+        variable.insert(variable.end(), x);
+    }
+    std::sort(variable.begin(), variable.end());
+    debug << variable[0];
+    get_coefficients(coefficients, n1, variable);
+    max = -1;
+    max_x = -1;
+    for (int i = 0; i <= n2; i++) {
+        double x = get_value(coefficients, variable, a + i * h, n2);
+        double y = get_value(a + i * h);
+        debug << a + i * h << "= x\t\t\t" << std::abs(y-x) << std::endl;
         if (std::abs(y - x) > max) {
             max = std::abs(y - x);
             max_x = a + i * h;
